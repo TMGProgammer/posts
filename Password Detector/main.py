a8f9e3282c5d3ce33a00e4d772d896ea1e6f8a74
Tbm Tahmid <turbomite95xmc@gmail.com>
2026-08-11 17:06:00 +0600

    Implement password protection with link access

--- Password Detector/main.py
+++ Password Detector/main.py
@@ -1,1 +1,17 @@
+password = "12345qwe"
+links = ["https://github.com", "https://facebook.com", "https://google.com"]
+attempts = 3
 
+while attempts > 0:
+  user_input = input("Please enter your password: ")
+  if user_input == password:
+    print("Access Granted")
+    for link in links:
+      print(link)
+      break
+  else:
+    attempts -= 1
+    if attempts > 0:
+      print(f"Incorrect password! {attempts} attempts left.")
+    else:
+      print("Access denied!")
